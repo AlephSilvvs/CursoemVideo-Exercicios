b3b2b3150c41a52839c894332efdc39fc7b9c8e8
Alefe Silva <alefenelson@gmail.com>
2025-08-20 16:37:49 -0300

    01

---
+++ ex016.py
@@ -0,0 +1,13 @@
+"""Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira."""
+
+# Importando biblioteca
+"""from math import trunc
+
+# Pergunta o valor e mostra na tela
+num = float(input('Digite um valor: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {trunc(num)} ') # Operação feita dentro do format"""
+
+
+# Outra forma de fazer, sem importar biblioteca
+num = float(input('Digite um valor: '))
+print (f'O valor digitado foi {num} e a sua porção inteira é {int(num)}')

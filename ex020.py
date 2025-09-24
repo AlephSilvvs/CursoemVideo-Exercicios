0675a8f3d2070593a241d44a43f65889d9153742
Alefe Silva <alefenelson@gmail.com>
2025-09-24 17:28:34 -0300

    01

---
+++ ex020.py
@@ -0,0 +1,17 @@
+"""O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada."""
+
+# Importa a biblioteca pra randomizar
+from random import shuffle             #import random
+
+# Pergunta os nome
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+
+# Lista e randomiza
+lista = [n1, n2, n3, n4]
+shuffle(lista)                        #random.shuffle(lista)
+
+print(f'A ordem de apresentação será {lista}')

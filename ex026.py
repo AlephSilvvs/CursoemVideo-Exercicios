8eeb586d7031fa43fe2f0ac56043ef729f94dcd0
Alefe Silva <alefenelson@gmail.com>
2025-10-28 11:21:54 -0300

    02

--- ex026.py
+++ ex026.py
@@ -8,7 +8,7 @@ frase = str(input('Digite uma frase: ')).upper().strip()
 print(f'A letra A aparece {frase.count('A')} vezes na frase.')
 
 # Localiza a prinmeira letra 'A' na frase
-print(f'A primeiroa letra A apareceu na posição {frase.find('A')+1}')
+print(f'A primeira letra A apareceu na posição {frase.find('A')+1}')
 
 # Localiza a ultima letra 'A'
 print(f'A última letra A apareceu na posição {frase.rfind('A')+1}')

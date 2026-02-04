87e318beb5e02c435e05cfc7b8bbe343582bffc5
Alefe Silva <alefenelson@gmail.com>
2026-02-04 16:58:56 -0300

    01

--- ex036.py
+++ ex036.py
@@ -3,18 +3,24 @@ o valor da casa, o salário do comprador e em quantos anos ele vai pagar
 
 Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado"""
 
-# Pergunta ao usuario
-casa = int(input('Qual o valor da casa: R$'))
-salario = int(input('Qual o seu salário: R$'))
+
+# Pergunta ao usuário
+casa = float(input('Qual o valor da casa: R$'))
+salario = float(input('Qual o salário do comprador: R$'))
 anos = int(input('Quantos anos de financiamento? '))
 
-# Transforma anos em meses
-prestacao = casa / (anos*12)   # A pergunta das parcelas está em anos, mas o cálculo faz com meses
+
+# Calcula a prestação e transforma anos em meses
+prestacao = casa / (anos * 12)   # A pergunta das parcelas está em anos, mas o cálculo faz com meses
+
+
+# Mostra a usuário
+print (f'Para pagar uma casa de R${casa:.2f} em {anos}', end='')
+print (f' a prestação será de R${prestacao:.2f}')
+
 
 # Condição simples
 if prestacao <= 30 / 100 * salario:
-    print(f'Você terá que pagar R${prestacao:.2f} por mês durante {anos} anos')
     print('Empréstimo pode ser CONCEDIDO!')
 else:
     print(f'Emprestimo negado')
-
